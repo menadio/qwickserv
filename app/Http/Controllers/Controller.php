73339816e189a5f72c4c039cc2ccaa75e71d7b7b
Elomena Idise <menadio1@gmail.com>
2021-01-23 14:16:41 +0100

    register, login, logout & default response added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,71 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Send successful response
+     * 
+     * @param $data
+     * @param $message
+     * @return \Illuminate\Http\Response
+     */
+    public function successResponse($data = null, $message = null, $httpCode = 200)
+    {
+        if (is_null($data)) {
+
+            return response()->json([
+                'success'   => true,
+                'message'   => $message
+            ], $httpCode);
+
+        } else {
+
+            return response()->json([
+                'success'   => true,
+                'data'      => $data,
+                'message'   => $message
+            ], $httpCode);
+
+        }
+    }
+
+    /**
+     * Send error response
+     * 
+     * @param $error
+     * @param $message
+     * @return \Illuminate\Http\Response
+     */
+    public function errorResponse($error = null, $message = null, $httpCode = 400)
+    {
+        if (is_null($error)) {
+
+            return response()->json([
+                'success'       => false,
+                'message'       => $message
+            ], $httpCode);
+
+        } else {
+
+            return response()->json([
+                'success'       => false,
+                'error'         => $error,
+                'message'       => $message
+            ], $httpCode);
+
+        }
+    }
+
+    /**
+     * Send server error response
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function serverError()
+    {
+        return response()->json([
+            'success'   => false,
+            'message'   => 'Oops! That shouldn\'t happen, please try again later.'
+        ], 500);
+    }
 }
